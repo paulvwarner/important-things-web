b8e79d7fab5ddff25412158c2a306916a80b472a
Paul Warner <paul.vincent.warner@gmail.com>
2022-01-29 16:45:25 -0600

    move some FCM config to encrypted credentials (changed server key being used), set up prod DB config in encrypted credentials, ignore FCM json file in repo (made a new one)

--- app/helpers/important_things_helper.rb
+++ app/helpers/important_things_helper.rb
@@ -43,8 +43,8 @@ module ImportantThingsHelper
 
   def send_notification_for(important_thing)
     fcm = FCM.new(
-      'AAAAHQDVQCw:APA91bGaPNPRz0-lxPAvidhI2-65rLJ8_lE7dvHT4Vwr61ByjiWtxqPCDhYULpzBXU4H5aH8gxLTDovaepjXYGJOWhOPcWfxkzENp26LndReBdhueXNNcEkRrWyjQmrJ6It69RgoHSRn',
-      File.join(Rails.root, 'config', 'importantthingsmobile-firebase-adminsdk-a312w-2ff251740e.json'),
+      Rails.application.credentials.fcm_server_key,
+      File.join(Rails.root, 'config', 'importantthingsmobile-firebase-adminsdk-268dc30ff6ae.json'),
       'importantthingsmobile'
     )
 
